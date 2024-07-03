c196948bee9d39b455888890baa5ddd3763d44b9
NatieleCosta <natisouza806@gmail.com>
2024-07-02 23:08:08 -0300

    Carregamento de Dados

---
+++ Desafio/desafio.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+df = pd.read_csv('TrilhaDadosJR-JUN15/Desafio/vendas_cursos.csv')
+
+print("Primeiras linhas do conjunto de dados:")
+print(df.head())
+
+print("\nInformações básicas do conjunto de dados:")
+print(df.info())
